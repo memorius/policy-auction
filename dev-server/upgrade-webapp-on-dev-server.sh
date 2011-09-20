c3d7b119f0c3a00db0b0e4f478983525f4ce6046
Nick Clarke <memorius@gmail.com>
2011-09-20 22:37:48 +1200

    Add script to build war, copy to dev server and upgrade it

---
+++ dev-server/upgrade-webapp-on-dev-server.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+trap_err () {
+    err=$?
+
+    echo 1>&2 "$0: in command $BASH_COMMAND
+Error $err occurred, aborting."
+}
+
+print_error () {
+    echo 1>&2 "$0: $1"
+}
+
+exit_with_error () {
+    local message="$1"
+    local exit_code=$2
+
+    print_error "$message"
+    if [ -z "$exit_code" ]; then
+        exit_code=1
+    fi
+    exit $exit_code
+}
+
+trap trap_err ERR && set -o errexit
+
+echo -n "Enter username on target server: "
+read username
+
+echo -n "Enter target hostname: "
+read server_host
+
+if [ -z "$username" ] || [ -z "$server_host" ]; then
+    exit_with_error "Empty host or username" 2
+fi
+
+cd ..
+mvn clean package
+
+echo "Copying war to target server..."
+scp target/policy-auction.war "${username}@${server_host}:~/policy-auction.war"
+
+echo "...copied ok, installing..."
+ssh -t "${username}@${server_host}" sudo "~/upgrade-policy-auction-war.sh"
